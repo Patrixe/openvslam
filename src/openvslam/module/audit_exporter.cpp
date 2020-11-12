6596d81981167a67e26d0692bb7981418ddf4552
Patrick <patrick.liedtke@volkswagen.de>
2020-11-12 11:11:26 +0100

    Add exporter class to write out information created during the SLAM

---
+++ src/openvslam/module/audit_exporter.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Patrick Liedtke on 12.11.20.
+//
+
+#include "audit_exporter.h"
---
+++ src/openvslam/module/audit_exporter.h
@@ -0,0 +1,14 @@
+//
+// Created by Patrick Liedtke on 12.11.20.
+//
+
+#ifndef OPENVSLAM_AUDIT_EXPORTER_H
+#define OPENVSLAM_AUDIT_EXPORTER_H
+
+
+class audit_exporter {
+
+};
+
+
+#endif //OPENVSLAM_AUDIT_EXPORTER_H
